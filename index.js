5bab25d7382a4be14fd8befc555b5d8e18711389
Jed Northridge <git@jedcn.com>
2014-12-26 14:22:33 -0500

    Created index.js: it requires and exports js/*

---
+++ index.js
@@ -0,0 +1,18 @@
+var _ = require("underscore");
+var fs = require("fs");
+
+function exportAllInside(modulePath) {
+  var module = require(modulePath);
+  _.each(_.functions(module), function(funName) {
+    exports[funName] = module[funName];
+  });
+}
+
+function requireAndExportFilesIn(dir) {
+  var files = fs.readdirSync(dir);
+  _.each(files, function(file) {
+    exportAllInside("./" + dir + "/" + file);
+  });
+}
+
+requireAndExportFilesIn("js");
