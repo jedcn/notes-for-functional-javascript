44fa3717d65a9c8725069a2ed3a69792d2b34ea8
Jed Northridge <git@jedcn.com>
2014-12-31 16:31:57 -0500

    [chapter 8] Notes through page 189

--- js/chapter08.js
+++ js/chapter08.js
@@ -3,9 +3,10 @@
 //
 
 var _ = require("underscore");
+var importAll = require("./importAll.js").importAll;
+importAll("chapter02.js");
 
 function pipeline(seed /*, args */) {
-
   return _.reduce(_.rest(arguments),
                   function(value, fun) {
                     return fun(value);
@@ -14,4 +15,39 @@ function pipeline(seed /*, args */) {
                  );
 }
 
+function lift(answerFun, stateFun) {
+  return function(/* args */) {
+    var args = _.toArray(arguments);
+    return function(state) {
+      // answerFun takes state as first argument and then args of
+      // returned function
+      var appliedArgs = construct(state, args)
+      var ans = answerFun.apply(null, appliedArgs);
+
+      // Was a stateFun supplied? If so, pass it the state and use the
+      // result it returns. Otherwise use the result of answerFun
+      var s = stateFun ? stateFun(state) : ans;
+
+      // Finally, capture the result of answerFun in "answer" and the
+      // computed state in "state."
+      return { answer: ans, state: s};
+    };
+  };
+}
+
+function actions(acts, done) {
+  return function(seed) {
+    var init = { values: [], state: seed };
+    var intermediate = _.reduce(acts, function(stateObj, action) {
+      var result = action(stateObj.state);
+      var values = cat(stateObj.values, [result.answer]);
+      return { values: values, state: result.state };
+    }, init);
+    var keep = _.filter(intermediate.values, existy);
+    return done(keep, intermediate.state);
+  };
+}
+
+exports.actions = actions;
+exports.lift = lift;
 exports.pipeline = pipeline;
--- spec/chapter08Spec.js
+++ spec/chapter08Spec.js
@@ -42,4 +42,53 @@ describe("Chapter 8", function() {
       expect(result).toEqual(-99);
     });
   });
+
+  describe("lift", function() {
+    it("is like pipeline, but manages an intermediate object allowing for differing return values", function() {
+
+      function sqr(n) {
+        return n * n;
+      }
+      var mSqr2 = lift(sqr);
+
+      var note = jasmine.createSpy('note');
+      var mNote2 = lift(note, _.identity);
+
+      var mNeg2 = lift(function(n) { return -n });
+
+      var negativeSqrAction2 = actions([mSqr2(), mNote2(), mNeg2()], function(_, state) {
+        return state;
+      });
+
+      expect(negativeSqrAction2(100)).toEqual(-10000);
+      expect(note).toHaveBeenCalled();
+    });
+  });
+  describe("push + pop", function() {
+
+    it("represents a sequence of stack events ", function() {
+      var timesPushCalled = 0;
+      var push = lift(function(stack, e) {
+        timesPushCalled++;
+        return construct(e, stack)
+      });
+
+      var pop = lift(_.first, _.rest);
+
+      var stackAction = actions([push(1),
+                                 push(2),
+                                 pop()],
+                                function(values, state) {
+                                  return values;
+                                });
+      expect(timesPushCalled).toBe(0);
+      var result = stackAction([]);
+
+      var theQueueWithJustOne = [1];
+      var theQueueWithOneAndTwo = [2, 1];
+      var theResultOfPoppingTheTwo = 2;
+      expect(result).toEqual([theQueueWithJustOne, theQueueWithOneAndTwo, theResultOfPoppingTheTwo]);
+      expect(timesPushCalled).toBe(2);
+    });
+  });
 });
